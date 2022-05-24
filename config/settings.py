f708a6084f19a590a21ac93c300032fc8d8ea1c8
Batyrhan21 <baatyr3030@gmail.com>
2022-05-24 10:17:51 +0600

    auth backends and create usermodel app

--- config/settings.py
+++ config/settings.py
@@ -58,6 +58,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
