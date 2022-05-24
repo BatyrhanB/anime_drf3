6e5a2ffe1c03c93f3673074c2b97648dcdcdd514
Batyrhan21 <baatyr3030@gmail.com>
2022-05-24 10:19:49 +0600

    add usermodel model

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,8 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+AUTH_USER_MODEL = 'usermodel.User'
+
 ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
--- usermodel/models.py
+++ usermodel/models.py
@@ -1,3 +1,86 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.postgres.fields import CIEmailField
+from django.core.mail import send_mail
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+# from usermodel.managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    An abstract base class implementing a fully featured User model with
+    admin-compliant permissions.
+    Username and password are required. Other fields are optional.
+    """
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = CIEmailField(
+        _('Email Address'),
+        unique=True,
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+
+    first_name = models.CharField(_('First Name'), max_length=255, blank=True)
+    last_name = models.CharField(_('Last Name'), max_length=255, blank=True)
+
+    is_staff = models.BooleanField(
+        _('Staff Status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+
+    is_active = models.BooleanField(
+        _('Active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
+
+    # Audit Values
+    is_email_confirmed = models.BooleanField(
+        _('Email Confirmed'),
+        default=False
+    )
+    date_joined = models.DateTimeField(
+        _('Date Joined'),
+        default=timezone.now
+    )
+
+    # objects = UserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'first_name',
+        'last_name'
+    ]
+
+    class Meta:
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        return f"{self.first_name} {self.last_name}"
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """Send an email to this user."""
+        send_mail(subject, message, from_email, [self.email], **kwargs)
