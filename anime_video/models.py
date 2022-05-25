52aeb6ce97841f4c79d51369be7ba37471e3715a
Batyrhan21 <baatyr3030@gmail.com>
2022-05-25 19:35:02 +0600

    flash video model

--- anime_video/models.py
+++ anime_video/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime
+from django.conf import settings
 from django.core.validators import FileExtensionValidator
 
 
@@ -109,3 +110,34 @@ class EmbedVideo(Video):
         blank=True,
         help_text="Use the video embed code instead of the url if your frontend does not support embedding with the URL only."
     )
+
+class FlashVideo(Video):
+    original_file = models.FileField(
+        upload_to="videos/flash/source/",
+        null=True,
+        blank=True,
+        help_text="Make sure that the video you are uploading has a audo bitrate of at least 16. The encoding wont function on a lower audio bitrate."
+    )
+
+    flv_file = models.FileField(
+        upload_to="videos/flash/flv/",
+        null=True,
+        blank=True,
+        help_text="If you already have an encoded flash video, upload it here (no encoding needed)."
+    )
+
+    thumbnail = models.ImageField(
+        blank=True,
+        null=True, 
+        upload_to="videos/flash/thumbnails/",
+        help_text="If you uploaded a flv clip that was already encoded, you will need to upload a thumbnail as well. If you are planning use django-video to encode, you dont have to upload a thumbnail, as django-video will create it for you"
+    )
+
+    encode = models.BooleanField(
+        default=False,
+        help_text="Encode or Re-Encode the clip. If you only wanted to change some information on the item, and do not want to encode the clip again, make sure this option is not selected."
+    )
+
+    def get_player_size(self):
+        size = getattr(settings, 'VIDEOSTREAM_SIZE', '320x240').split('x')
+        return "width: %spx; height: %spx;" % (size[0], size[1])
