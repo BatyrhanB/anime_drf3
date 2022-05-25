1a60999bee756fddb3b2260b65ddc9ee435bd48c
Batyrhan21 <baatyr3030@gmail.com>
2022-05-25 14:15:10 +0600

    video model fixs

--- anime_video/models.py
+++ anime_video/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import datetime
 from django.core.validators import FileExtensionValidator
 
 
@@ -11,15 +12,60 @@ class VideoCategory(models.Model):
     description = models.TextField(null=True, blank=True)
 
 
+    class Meta:
+        verbose_name = "Video Categories"
+        verbose_name_plural = "Video Categories"
+
+    def __unicode__(self):
+        return "%s" % self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('videostream_category_detail', [self.slug])
+
+
+
+
 class Video(models.Model):
     title = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    slug = models.SlugField(unique=True,
+        help_text="A url friendly slug for the video clip.")
     image = models.ImageField(upload_to='image/')
     file = models.FileField(
         upload_to='video/',
         validators=[FileExtensionValidator(allowed_extensions=['mp4'])]
     )
+    allow_comments = models.BooleanField(default=False)
+    is_public = models.BooleanField(default=False)    
     create_at = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Video"
+        verbose_name_plural = "Video"
+        ordering = ('-publish_date', '-created_date')
+        get_latest_by = 'publish_date'
+
 
     def __str__(self):
         return self.title
+
+    def __unicode__(self):
+        return "%s" % self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('videostream_video_detail', (), { 
+            'year': self.publish_date.strftime("%Y"),
+            'month': self.publish_date.strftime("%b"),
+            'day': self.publish_date.strftime("%d"), 
+            'slug': self.slug 
+        })
+
+    def save(self, *args, **kwargs):
+        self.modified_date = datetime.now()
+        if self.publish_date == None and self.is_public:
+            self.publish_date = datetime.now()
+        super(Video, self).save(*args, **kwargs)
