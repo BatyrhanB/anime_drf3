052fba8fa4a2d14e02db3fe70206d3ab733bd52d
Batyrhan21 <baatyr3030@gmail.com>
2022-05-25 19:37:56 +0600

    admin video category

--- anime_video/admin.py
+++ anime_video/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+class VideoCategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'slug']
