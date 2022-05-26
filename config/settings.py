b726d23b148aeb206cbec8213d03daecac4b6e14
Batyrhan21 <baatyr3030@gmail.com>
2022-05-26 15:46:30 +0600

    add logging settings

--- config/settings.py
+++ config/settings.py
@@ -220,3 +220,19 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        },
+    },
+    'loggers': {
+        '': { 
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
