b1a16ecf28e55afd97254a5530c8c0c457ff1078
Batyrhan21 <baatyr3030@gmail.com>
2022-05-26 16:42:40 +0600

    add templates dirs base dir

--- config/settings.py
+++ config/settings.py
@@ -92,8 +92,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
