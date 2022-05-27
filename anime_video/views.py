98fd6a2940fbddc1eb41ec970f3b4d311d9d6ce7
Batyrhan21 <baatyr3030@gmail.com>
2022-05-27 17:36:21 +0600

    category list and detail views/urls

--- anime_video/views.py
+++ anime_video/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Video, VideoCategory
-from . serializers import VideoSerializer
+from . serializers import VideoSerializer, CategorySerializer
 class VideoList(ListAPIView):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
@@ -15,5 +15,9 @@ class VideoDetail(RetrieveAPIView):
 
 class CategoryList(ListAPIView):
     queryset = VideoCategory.objects.all()
-    serializer_class = VideoSerializer
+    serializer_class = CategorySerializer
+
+class CategoryDetail(RetrieveAPIView):
+    queryset = VideoCategory.objects.all()
+    serializer_class = CategorySerializer
 
