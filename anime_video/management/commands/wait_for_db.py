505edbfee85fb69dd62969504846ff8a91adc1c1
Batyrhan21 <baatyr3030@gmail.com>
2022-05-31 15:08:38 +0600

    management command wait for db

---
+++ anime_video/management/commands/wait_for_db.py
@@ -0,0 +1,17 @@
+import time
+from django.db import connections
+from django.db.utils import OperationalError
+from django.core.management import BaseCommand
+ 
+class Command(BaseCommand): 
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for database...')
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections['default']
+            except OperationalError:
+                self.stdout.write('Database unavailable, waititng 1 second...')
+                time.sleep(1)
+ 
+        self.stdout.write(self.style.SUCCESS('Database available!'))
