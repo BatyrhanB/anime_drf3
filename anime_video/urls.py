444e5cb458477b16d0a0cf1407acc2d57307627b
Batyrhan21 <baatyr3030@gmail.com>
2022-06-23 17:45:33 +0600

    add tests directory

--- anime_video/urls.py
+++ anime_video/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('anime/<int:pk>/', views.AnimeDetail.as_view(), name='anime-detail'),
     path('video/', cache_page(30)(views.VideoList.as_view()), name='video-list'),
     path('video/<int:pk>/', views.VideoDetail.as_view(), name='video-detail'),
-    path('actors/', views.PersonageList.as_view(), name='personage-list'),
+    path('actors/', cache_page(30)(views.PersonageList.as_view()), name='personage-list'),
     path('actors/<int:pk>/', views.PersonageDetail.as_view(), name='personage-detail'),
-    path('genre/', views.GenreList.as_view(), name='genre-list'),
+    path('genre/', cache_page(30)(views.GenreList.as_view()), name='genre-list'),
     path('genre/<int:pk>/', views.GenreDetail.as_view(), name='genre-detail'),
-    path('shots/', views.VideoShotsList.as_view(), name='shots-list'),
+    path('shots/', cache_page(30)(views.VideoShotsList.as_view()), name='shots-list'),
 ]
