9bb53089c2ac063694dc825d93370a72b741bcc0
Batyrhan21 <baatyr3030@gmail.com>
2022-08-17 15:14:50 +0600

    added celery, broker redis and flower monitoring

--- config/celery.py
+++ config/celery.py
@@ -8,10 +8,14 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
  
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
-# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
+@app.task
+def divide(x, y):
+    import time
+    time.sleep(5)
+    return x / y
 
 @app.task(bind=True)
 def debug_task(self):
